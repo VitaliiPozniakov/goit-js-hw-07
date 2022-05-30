07c4d4eaa4dfc83e082389228c47f43b96f0302f
VitaliiPozniakov <vertalen89@gmail.com>
2022-05-30 17:34:56 +0300

    All gallery to task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,58 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryRef  = document.querySelector(`.gallery`);
+
+
+const galleryElements = makeGalleryMarkup(galleryItems);
+
+galleryRef.insertAdjacentHTML(`afterbegin`, galleryElements)
+
+
+galleryRef.addEventListener(`click`, onGalleryRefClick)
+
+
+
+function makeGalleryMarkup (galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+        <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+        `
+    }).join("")
+}
+
+function onGalleryRefClick (e) {
+    e.preventDefault();
+
+    if (e.target.nodeName !== "IMG") {
+        return;
+      }
+      const urlBigImg = e.target.dataset.source;
+console.log(urlBigImg)
+
+
+
+const instance = basicLightbox.create(`
+    <img src="${urlBigImg}" width="800" height="600">
+`)
+
+instance.show()
+}
+
+
+
+
+
+
+
