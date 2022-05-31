eb34c0160b6286e0a08a33da59988b2d6525ffd2
VitaliiPozniakov <vertalen89@gmail.com>
2022-05-31 14:50:15 +0300

    1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,10 +1,11 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-console.log(galleryItems);
+// console.log(galleryItems);
 
 const galleryRef = document.querySelector(`.gallery`);
 
 const galleryElements = makeGalleryMarkup(galleryItems);
+
 galleryRef.insertAdjacentHTML(`afterbegin`, galleryElements);
 
 galleryRef.addEventListener(`click`, onGalleryRefClick);
@@ -30,7 +31,5 @@ function onGalleryRefClick(e) {
     return;
   }
 
-  const urlBigImg = e.target.dataset.source;
-
-  var lightbox = new SimpleLightbox(".gallery a", { captionDelay: 250 });
+  const lightbox = new SimpleLightbox(".gallery a", { captionDelay: 250 });
 }
